8bb52da97554a4b1215a4b78253a5ce551bd72da
MyatHsuMon <myathsumon554@gmail.com>
2019-12-26 20:56:01 +0630

    cancel request method

--- databaseOperations/userCRUD.js
+++ databaseOperations/userCRUD.js
@@ -6,14 +6,13 @@ const signup = async (user) => {
     return await User.create(user);
 }
 const login = async (user) => {
-    return await User.findOne(user, { email: 0, password: 0 });
-
+    return await User.findOne(user);
 }
 const createPost = async (postedValue, userId) => {
     return await User.updateOne({ _id: userId }, { $push: { posts: postedValue } });
 }
 const search = async (searchValue) => {
-    const result = await User.find({ $text: { $search: searchValue } },{name:1, relationship:1,friendSuggests:1, });
+    const result = await User.find({ $text: { $search: searchValue } },{name:1, relationship:1,friendSuggests:1,posts:1 });
     return result;
 }
 const searchProfile = async (data) =>{
@@ -32,15 +31,15 @@ const addFriend = async (data) =>{
      await User.updateOne({_id:data.senderId},{$push:{friendRequests:receiver}, $inc:{numberOfFriendRequests:1}});
      await User.updateOne({_id:data.receiverId},{$addToSet:{friendSuggests:sender},  $inc:{numberOfFriendSuggests:1}});
      const result1 = await User.findOne({_id:data.senderId},{friendRequests:1,name:1,numberOfFriendRequests:1});
-     const result2 = await User.findOne({_id:data.receiverId},{friendSuggests:1,name:1,numberOfFriendSuggests:1});
-    
-
-
+     const result2 = await User.findOne({_id:data.receiverId},{friendSuggests:1,name:1,numberOfFriendSuggests:1}); 
 }
 const cancelRequest = async (data)=>{
+    await User.updateOne({_id:data.senderId}, {$pull:{friendRequests:{receiverId:data.receiverId},$inc:{numberOfFriendRequests:-1}}});
     await User.updateOne({_id:data.receiverId},{$pull:{friendSuggests:{senderId:data.senderId}}, $inc:{numberOfFriendSuggests:-1}});
-    const result = await User.findOne({_id:data.receiverId},{name:1,friendSuggests:1});
-    console.log("Result is ,", result);
+    const result = await User.findOne({_id:data.receiverId},{name:1,friendSuggests:1, friends:1});
+    const result2 = await User.findOne({_id:data.senderId},{name:1,friendRequests:1,friends:1});
+    console.log("sender information is ", result2);
+    console.log("receiver information  is ,", result);   
 }
 const confirm = async (data) =>{
     const receiver ={
@@ -55,7 +54,7 @@ const confirm = async (data) =>{
     await User.updateOne({_id:data.receiverId},{$pull:{friendSuggests:{senderId:data.senderId}},$push:{friends:sender}});// { $addToSet: { tags: "camera"  } }
     const result=await User.findOne({_id:data.senderId},{name:1,friends:1,friendRequests:1});
     const result2=await User.findOne({_id:data.receiverId},{name:1,friends:1,friendSuggests:1});
-    console.log("sender is",result)
+    console.log("sender is",result);
     console.log("receiver is ", result2);
 }
 module.exports = {
