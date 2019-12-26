55440e03935df04bac1b25ccd29c33a780f66ecc
PhyoLwin <islatt.openlight@gmail.com>
2019-12-26 21:35:17 +0630

    done

--- databaseOperations/userCRUD.js
+++ databaseOperations/userCRUD.js
@@ -13,7 +13,7 @@ const createPost = async (postedValue, userId) => {
     return await User.updateOne({ _id: userId }, { $push: { posts: postedValue } });
 }
 const search = async (searchValue) => {
-    const result = await User.find({ $text: { $search: searchValue } },{name:1, relationship:1,friendSuggests:1, friendRequests:1 });0
+    const result = await User.find({ $text: { $search: searchValue } },{name:1, relationship:1,friendSuggests:1, friendRequests:1, friends:1});0
     return result;
 }
 const searchProfile = async (data) =>{
@@ -21,21 +21,12 @@ const searchProfile = async (data) =>{
     return await User.findOne({_id:data.id},{name:1,posts:1});
 }
 const addFriend = async (data) =>{
-    const receiver ={
-        receiverId:data.receiverId,
-        receiverName:data.receiverName
-    };
-    const sender ={
-        senderId :data.senderId,
-        senderName: data.senderName
-    }
-     await User.updateOne({_id:data.senderId},{$push:{friendRequests:receiver}, $inc:{numberOfFriendRequests:1}});
-     await User.updateOne({_id:data.receiverId},{$addToSet:{friendSuggests:sender},  $inc:{numberOfFriendSuggests:1}});
-     const result1 = await User.findOne({_id:data.senderId},{friendRequests:1,name:1,numberOfFriendRequests:1});
-     const result2 = await User.findOne({_id:data.receiverId},{friendSuggests:1,name:1,numberOfFriendSuggests:1});
+   
+     await User.updateOne({_id:data.senderId},{$push:{friendRequests:data.receiverId}, $inc:{numberOfFriendRequests:1}});
+     await User.updateOne({_id:data.receiverId},{$addToSet:{friendSuggests:data.senderId},  $inc:{numberOfFriendSuggests:1}});
+    //  const result1 = await User.findOne({_id:data.senderId},{friendRequests:1,name:1,numberOfFriendRequests:1});
+    //  const result2 = await User.findOne({_id:data.receiverId},{friendSuggests:1,name:1,numberOfFriendSuggests:1});
     
-
-
 }
 const cancelRequest = async (data)=>{
     await User.updateOne({_id:data.receiverId},{$pull:{friendSuggests:{senderId:data.senderId}}, $inc:{numberOfFriendSuggests:-1}});
