c74eb5076a1837c25d3b6472536ca7339784abbd
MyatHsuMon <myathsumon554@gmail.com>
2019-12-28 11:34:56 +0630

    Friends

--- databaseOperations/userCRUD.js
+++ databaseOperations/userCRUD.js
@@ -8,20 +8,20 @@ const signup = async (user) => {
 const login = async (user) => {
     return await User.findOne(user);
 }
-// const createPost = async (data) => {
-//      await User.updateOne({ _id: data.id },{ $push: { posts: data.postedValue } });
-//      const result = await User.findOne({_id : data.id});
-//      console.log("Result is ",result);
-//      return await result;
-// }
+const createPost = async (data) => {
+     await User.updateOne({ _id: data.id },{ $push: { posts: data.postedValue } });
+     const result = await User.findOne({_id : data.id});
+     console.log("Result is ",result);
+     return await result;
+}
 const search = async (searchValue) => {
     const result = await User.find({ $text: { $search: searchValue } },{name:1, posts:1, relationship:1,friendSuggests:1, friendRequests:1, friends:1});0
     return result;
 }
-// const searchProfile = async (data) =>{
-//     console.log("id",data.id);
-//     return await User.findOne({_id:data.id},{name:1,posts:1});
-// }
+const searchProfile = async (data) =>{
+    console.log("id",data.id);
+    return await User.findOne({_id:data.id},{name:1,posts:1});
+}
 const addFriend = async (data) =>{
     const sender = {
         senderId: data.senderId,
@@ -38,7 +38,6 @@ const removeFriendSuggestsNoti = async(id) =>{
 }
 
 const cancelRequest = async (data)=>{
-    console.log('cancel request works in db operation')
     await User.updateOne({_id:data.senderId}, {$pull:{friendRequests:{$in:[data.receiverId]}}});
     await User.updateOne({_id:data.receiverId},{$pull:{friendSuggests:{$in:[data.senderId]},
         friendSuggestsForNoti:{senderId:data.senderId}}});
@@ -49,23 +48,37 @@ const cancelRequest = async (data)=>{
     console.log("receiver information  is ,", receiver); 
     return receiver; 
 }
-const confirm = async (data) =>{
-    const receiver ={
+
+const acceptRequest = async (data) =>{
+    const receiver = {
         receiverId:data.receiverId,
         receiverName:data.receiverName
     };
-
-    const sender ={
+    const sender = {
         senderId :data.senderId,
         senderName: data.senderName
     }
-    await User.updateOne({_id:data.senderId},{$pull:{friendRequests:{receiverId:data.receiverId}},$push:{friends:receiver}});
-    await User.updateOne({_id:data.receiverId},{$pull:{friendSuggests:{senderId:data.senderId}},$push:{friends:sender}});// { $addToSet: { tags: "camera"  } }
-    const result=await User.findOne({_id:data.senderId},{name:1,friends:1,friendRequests:1});
-    const result2=await User.findOne({_id:data.receiverId},{name:1,friends:1,friendSuggests:1});
-    console.log("sender is",result);
-    console.log("receiver is ", result2);
+    await User.updateOne({_id:data.senderId},{$pull:{friendSuggests:{$in:[data.receiverId]},
+        friendSuggestsForNoti:{senderId:data.receiverId}},$push:{friends:data.receiverId}});
+    await User.updateOne({_id:data.receiverId},{$pull:{friendRequests:{$in:[data.senderId]}},
+     $push:{friends:data.senderId}});
+    const senderInfo=await User.findOne({_id:data.senderId},{name:1,friends:1,friendSuggestsForNoti:1});
+    const receiverInfo=await User.findOne({_id:data.receiverId},{name:1,friends:1,friendRequests:1});
+    console.log("sender information is",senderInfo);
+    console.log('/////////////////////////////////////////////////')
+    console.log("receiver information is ", receiverInfo);
+    return senderInfo;
 }
+
+// const getFriends = (data) =>{
+//     friendsWithIdAndName = data.friends.map(async(id) =>{
+//         name = await User.findOne({_id:id},{name:1});
+//         console.log("Name ", name)
+//         return name;
+//     })
+//     console.log("Friends with names", friendsWithIdAndName);
+//     return Promise.all(friendsWithIdAndName) ;
+// }
 
 
 module.exports = {
@@ -73,10 +86,11 @@ module.exports = {
     signup,
     login,
     search,
-    createPost,
-    searchProfile,
     addFriend,
     cancelRequest,
-    confirm,
-    removeFriendSuggestsNoti
+    acceptRequest,
+    createPost,
+    searchProfile,
+    removeFriendSuggestsNoti,
+    // getFriends
 }
