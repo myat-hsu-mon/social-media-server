92e5983cf728123c18657239d1e2d3f186e6bd80
MyatHsuMon <myathsumon554@gmail.com>
2019-12-29 07:01:06 +0630

    message

--- app.js
+++ app.js
@@ -10,70 +10,83 @@ const port = 3000;
 app.set('port', port)
 const server = http.createServer(app)
 const io = require('socket.io').listen(server)
-io.on('connection',  (socket)=>{
+io.on('connection', (socket) => {
     console.log('socket connected');
     console.log(socket.id)
-    socket.on('addFriend',async  (data)=>{
+    socket.on('addFriend', async(data) => {
         const notiData = await userCRUD.addFriend(data)
-        socket.emit(`${data.senderId}friendRequest`, {_id: data.receiverId,name:data.receiverName})
-        socket.broadcast.emit(`${data.receiverId}friendSuggestNoti`,notiData);
+        socket.emit(`${data.senderId}friendRequest`, { _id: data.receiverId, name: data.receiverName })
+        socket.broadcast.emit(`${data.receiverId}friendSuggestNoti`, notiData);
     })
 
-    socket.on('cancelRequest', async (data)=>{
+    socket.on('cancelRequest', async(data) => {
         const receiver = await userCRUD.cancelRequest(data);
-        socket.emit(`${data.senderId}canceledRequest`,receiver)
-        socket.broadcast.emit(`${data.receiverId}friendSuggestNoti`,receiver)
+        socket.emit(`${data.senderId}canceledRequest`, receiver)
+        socket.broadcast.emit(`${data.receiverId}friendSuggestNoti`, receiver)
     })
 
-    socket.on('removeFriendSuggestsNoti',async(id)=>{
-       const numberOfFriendSuggests = await userCRUD.removeFriendSuggestsNoti(id);
-       socket.emit(`${id}removedFriendSuggestsNoti`,numberOfFriendSuggests);
-    })    
+    socket.on('removeFriendSuggestsNoti', async(id) => {
+        const numberOfFriendSuggests = await userCRUD.removeFriendSuggestsNoti(id);
+        socket.emit(`${id}removedFriendSuggestsNoti`, numberOfFriendSuggests);
+    })
 
-    socket.on('acceptRequest',async (data)=>{
+    socket.on('acceptRequest', async(data) => {
         const sender = await userCRUD.acceptRequest(data);
-       socket.emit(`${data.senderId}acceptedRequest`,sender);
+        socket.emit(`${data.senderId}acceptedRequest`, sender);
         // socket.emit(data.receiverId,{_id:data.senderId,name:data.senderName})
     })
 
-    socket.on('friends',  async (data) =>{
+    socket.on('friends', async(data) => {
         const friendsWithIdAndName = await userCRUD.getFriends(data);
-        console.log("Friends with names ",friendsWithIdAndName);
-        socket.emit(`${data.id}friendsWithIdAndName`,friendsWithIdAndName);
+        console.log("Friends with names ", friendsWithIdAndName);
+        socket.emit(`${data.id}friendsWithIdAndName`, friendsWithIdAndName);
     })
 
-
-    socket.on('create post',async(data)=>{
-      
-       const userWithNewPost =await userCRUD.createPost(data);
-        socket.emit(data.id,userWithNewPost);
+    socket.on('create post', async(data) => {
+        const userWithNewPost = await userCRUD.createPost(data);
+        socket.emit(data.id, userWithNewPost);
     })
 
- })
+    socket.on('sendMessage', message =>{
+        console.log("send message:",message);
+        socket.emit('receivedMessage',message);
+    })
+
+    
 
 
-app.get('/',(req, res)=>{
-    console.log("Success");    
+
+
+
+
+
+
+
+})
+
+
+app.get('/', (req, res) => {
+    console.log("Success");
 });
-app.post('/signup',userController.signup);
-app.post('/login',userController.login);
+app.post('/signup', userController.signup);
+app.post('/login', userController.login);
 app.post('/post', userController.createPost);
-app.post('/search',userController.search);
-app.post('/profile',userController.searchProfile);
+app.post('/search', userController.search);
+app.post('/profile', userController.searchProfile);
 
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log('server is listening on port:', port)
 })
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/social-media",{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>{
-    console.log("Mongodb successfully connected");
-    
-})
-.catch(()=>{
-    console.log("Mongodb is not connected");
-})
-//module.exports = app;
+mongoose.connect("mongodb://localhost:27017/social-media", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongodb successfully connected");
+
+    })
+    .catch(() => {
+        console.log("Mongodb is not connected");
+    })
+    //module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,38 @@
 const userCRUD = require('../databaseOperations/userCRUD');
-const signup =async (req, res)=>{
+const signup = async(req, res) => {
     const user = req.body;
-    const {name, email, password } = user;
+    const { name, email, password } = user;
     // if(!name || !email || !password){
     //     return res.json({error:"Fill email or name or password"})
     // }else if(await userCRUD.existEmail(email)){
     //     return res.json({error:"Email already exists"});
     // }else{
-        return res.json(await userCRUD.signup(user));
+    return res.json(await userCRUD.signup(user));
     // }  
 }
 
-const login = async(req,res) =>{
-    const {email, password } = req.body;
+const login = async(req, res) => {
+    const { email, password } = req.body;
     // if(!email || !password){
     //     return res.json({error:"Fill email or password"});
     // }else
-     if(userCRUD.existEmail(email)){
-        return res.json(await userCRUD.login({email})) ;
-    }
-    else{
-        return res.json({error:"Email does not exist"});
+    if (userCRUD.existEmail(email)) {
+        return res.json(await userCRUD.login({ email }));
+    } else {
+        return res.json({ error: "Email does not exist" });
     }
 }
-const createPost = async (req,res) =>{
-    const {postedValue,id } = req.body;
-    return res.json(await userCRUD.createPost(postedValue,id));
+const createPost = async(req, res) => {
+    const { postedValue, id } = req.body;
+    return res.json(await userCRUD.createPost(postedValue, id));
 }
-const search = async (req,res)=>{
-    const searchValue = req.body;   
+const search = async(req, res) => {
+    const searchValue = req.body;
     const searchResult = await userCRUD.search(searchValue.searchValue);
     return res.json(searchResult);
-    
+
 }
-const searchProfile = async (req, res)=>{
+const searchProfile = async(req, res) => {
     return res.json(searchProfile);
 }
 
